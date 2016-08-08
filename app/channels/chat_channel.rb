9e2a74613916837ce8a1d466fe15cf2778d12d25
wizby0 <wizby0@naver.com>
2016-08-08 17:36:12 +0900

    test code for card owner player,deck,dummy  added

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -1,16 +1,16 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class ChatChannel < ApplicationCable::Channel
- 
+
  def subscribed
-    stream_from 'messages'
-  end
+  stream_from 'messages'
+end
 
-  def speak(data)
-    ActionCable.server.broadcast('messages',
-      message: render_message(data['message']))
-  end
+def speak(data)
+  ActionCable.server.broadcast('messages',
+    message: render_message(data['message']))
+end
 
-  def unsubscribed
+def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
 
@@ -30,23 +30,36 @@ class ChatChannel < ApplicationCable::Channel
   #   ApplicationController.render(partial: 'messages/message',
   #                                locals: { message: message })
   # end
-	def render_message(message)
-	  ApplicationController.render(
-	    partial: 'messages/message',
-	    locals: {
-	      message: message,
-	      username: current_user
-	    })
-	end
+  def render_message(message)
+   ApplicationController.render(
+     partial: 'messages/message',
+     locals: {
+       message: message,
+       username: current_user
+       })
+ end
 
-  def render_userListInfomessage()
+ def render_userListInfomessage()
 
     # players = Player.where(game_id:Game.last.id)
     players = Game.last.players
     count_number = Player.where(game_id: Game.last.id, role: nil).count #count player except deck and dummy
     ActionCable.server.broadcast('messages', players: players.each_with_index.map{|player, index| { index: index+1, name: player.user.name, status: player.status }}, count_number: count_number, system_info: "players_lists")
     
-   end
+  end
 
+  def render_cardListInfomessage()
+
+    players = Player.where(game_id:Game.last.id)
+    # each players do |player, index|
+    #   count_cards = Pockercard.where(player_id:player.id).count
+      
+    # end
+
+    cards = Pockercard.where(player_id:Player.last.id)
+    # count_number = Player.where(game_id: Game.last.id, role: nil).count #count player except deck and dummy
+    # ActionCable.server.broadcast('messages', players: players.each_with_index.map{|player, index| { index: index+1, name: player.user.name, status: player.status }}, count_number: count_number, system_info: "players_lists")
+    
+  end
 
 end
